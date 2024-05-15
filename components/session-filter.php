a0c4305bd8d0bb3136eb6841fd7ecdc950515750
jeANSdv <bazuev.4@yandex.ru>
2024-05-15 14:28:20 +0400

    fixing session

---
+++ components/session-filter.php
@@ -0,0 +1,78 @@
+<?php
+    
+?>
+
+<link rel="stylesheet" href="assets/css/seans.css">
+
+<div class="session-film-filter">
+
+    <h3 class="filter-main-text">ПОДБОР ПО ПАРАМЕТРАМ:</h3>
+
+    <div class="price-filt">
+        <h5 class="filt-second-text">Цена билета:</h5>
+        <input type='range' value='1' min='1' max='100' step='1'>
+    </div>
+
+    <h5 class="filt-second-text">Жанры:</h5>
+
+    <div class="xxx">
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">Ужасы</label>
+        </div>
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">Комедия</label>
+        </div>
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">Триллер</label>
+        </div>
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">Приключения</label>
+        </div>
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">Экшн</label>
+        </div>
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">История</label>
+        </div>
+    </div>
+
+    <h5 class="filt-second-text">Ограничение по возрасту:</h5>
+
+    <div class="age">
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">0+</label>
+        </div>
+
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">6+</label>
+        </div>
+
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">12+</label>
+        </div>
+
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">16+</label>
+        </div>
+
+        <div class="gal">
+            <input type="checkbox" value="0">
+            <label class="sort">18+</label>
+        </div>
+    </div>
+
+    <div class="price-filt">
+        <h5 class="filt-second-text">Длительность фильма:</h5>
+        <input type='range' value='1' min='1' max='100' step='1'>
+    </div>
+</div>
