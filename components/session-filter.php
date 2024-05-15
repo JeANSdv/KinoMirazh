0d07c7800c4ee008559d18d6410355ab8f26d8c8
jeANSdv <bazuev.4@yandex.ru>
2024-05-15 15:10:54 +0400

    working at session

--- components/session-filter.php
+++ components/session-filter.php
@@ -4,75 +4,77 @@
 
 <link rel="stylesheet" href="assets/css/seans.css">
 
-<div class="session-film-filter">
+<div class="film-filt-pos">
+    <div class="session-film-filter">
 
-    <h3 class="filter-main-text">ПОДБОР ПО ПАРАМЕТРАМ:</h3>
+        <h3 class="filter-main-text">ПОДБОР ПО ПАРАМЕТРАМ:</h3>
 
-    <div class="price-filt">
-        <h5 class="filt-second-text">Цена билета:</h5>
-        <input type='range' value='1' min='1' max='100' step='1'>
-    </div>
-
-    <h5 class="filt-second-text">Жанры:</h5>
-
-    <div class="xxx">
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">Ужасы</label>
-        </div>
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">Комедия</label>
-        </div>
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">Триллер</label>
-        </div>
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">Приключения</label>
-        </div>
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">Экшн</label>
-        </div>
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">История</label>
-        </div>
-    </div>
-
-    <h5 class="filt-second-text">Ограничение по возрасту:</h5>
-
-    <div class="age">
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">0+</label>
+        <div class="price-filt">
+            <h5 class="filt-second-text">Цена билета:</h5>
+            <input type='range' value='1' min='1' max='100' step='1'>
         </div>
 
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">6+</label>
+        <h5 class="filt-second-text">Жанры:</h5>
+
+        <div class="category-filt">
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">Ужасы</label>
+            </div>
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">Комедия</label>
+            </div>
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">Триллер</label>
+            </div>
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">Приключения</label>
+            </div>
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">Экшн</label>
+            </div>
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">История</label>
+            </div>
         </div>
 
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">12+</label>
+        <h5 class="filt-second-text">Ограничение по возрасту:</h5>
+
+        <div class="age-filt">
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">0+</label>
+            </div>
+
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">6+</label>
+            </div>
+
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">12+</label>
+            </div>
+
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">16+</label>
+            </div>
+
+            <div class="select-element">
+                <input type="checkbox" value="0">
+                <label class="sort">18+</label>
+            </div>
         </div>
 
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">16+</label>
+        <div class="price-filt">
+            <h5 class="filt-second-text">Длительность фильма:</h5>
+            <input type='range' value='1' min='1' max='100' step='1'>
         </div>
-
-        <div class="gal">
-            <input type="checkbox" value="0">
-            <label class="sort">18+</label>
-        </div>
-    </div>
-
-    <div class="price-filt">
-        <h5 class="filt-second-text">Длительность фильма:</h5>
-        <input type='range' value='1' min='1' max='100' step='1'>
     </div>
 </div>
